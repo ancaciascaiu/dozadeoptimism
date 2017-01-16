1c37842f39504c3dcd97037f10e2410bd8f9ffed
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2017-01-16 12:02:05 -0600

    add route for articles in config/routes

--- dozadeoptimism/app/models/article.rb
+++ dozadeoptimism/app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ActiveRecord::Base
 	belongs_to :volunteer, { :class_name => :User }
+
+	validates :user_id, presence: true
+	validates :content, presence: true, length: { maximum: 140 }
 end
