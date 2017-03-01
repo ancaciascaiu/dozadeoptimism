b19714e7afd65861cd4ec2758890d63f65aeee85
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2017-02-28 22:12:54 -0600

    update associations between users and articles

--- dozadeoptimism/app/models/article.rb
+++ dozadeoptimism/app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
-	belongs_to :volunteer, { :class_name => :User }
+	belongs_to :volunteer, class_name: :User, foreign_key: :volunteer_id 
 
-	validates :user_id, presence: true
-	validates :content, presence: true, length: { maximum: 140 }
+	validates :volunteer_id, presence: true
+	validates :title, :content, presence: true, length: { maximum: 5000 }
 end
