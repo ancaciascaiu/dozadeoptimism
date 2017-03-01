0af64dbfff9a010be05ad932ad69642f747b8903
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2017-02-28 22:20:01 -0600

    add info to reference users

--- dozadeoptimism/db/migrate/20170116005359_create_articles.rb
+++ dozadeoptimism/db/migrate/20170116005359_create_articles.rb
@@ -3,7 +3,7 @@ class CreateArticles < ActiveRecord::Migration
     create_table :articles do |t|
       t.string :title
       t.string :content
-      t.integer :volunteer_id
+      t.integer :volunteer_id, references: :users, index: true
 
       t.timestamps null: false
     end
